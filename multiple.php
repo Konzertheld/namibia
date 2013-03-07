6e8f17898768cdb691ef6d910b787d9d9cbded40
Konzertheld <christian.gredig@gmail.com>
2013-03-07 20:07:10 +0100

    Fix an unclosed div in the multiple view that caused display errors

--- multiple.php
+++ multiple.php
@@ -1,4 +1,4 @@
-<div class="multiple post <?php echo Post::type_name($content->content_type[0]); ?>
+<div class="multiple post <?php echo Post::type_name($content->content_type[0]); ?>">
 	<div class="postmeta">
 		<h2 class="postmeta-title"><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title_out; ?>"><?php echo $content->title_out; ?></a></h2>
 	</div>
