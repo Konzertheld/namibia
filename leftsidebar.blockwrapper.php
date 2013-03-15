6af05e8c5d691dddedc7c0e0700ca000278a44a0
Konzertheld <christian.gredig@gmail.com>
2013-03-15 15:23:27 +0100

    Add titles to blocks

--- leftsidebar.blockwrapper.php
+++ leftsidebar.blockwrapper.php
@@ -1,5 +1,6 @@
 					<div class="menu_links<?php if(!$block->_first): ?>_alternativ<?php endif; ?>"><div class="menu_links_02"><div class="menu_links_01"><div class="menu_links_03">
 					<div class="menu_links_content">
+						<?php if($block->_show_title) echo '<h2>' . $block->title . '</h2>'; ?>
 						<?php echo $content; ?>
 					</div>
 					</div></div></div></div>
--- rightsidebar.blockwrapper.php
+++ rightsidebar.blockwrapper.php
@@ -1,5 +1,6 @@
 					<div class="menu_rechts"><div class="menu_rechts_02"><div class="menu_rechts_01"><div class="menu_rechts_03">
 					<div class="menu_rechts_content" id="menu_rechts_content">
+						<?php if($block->_show_title) echo '<h2>' . $block->title . '</h2>'; ?>
 						<?php echo $content; ?>
 					</div>
 					</div></div></div></div>
